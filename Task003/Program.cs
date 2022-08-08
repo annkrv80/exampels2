8dd99c2e94e0b817b96b8d957968f472a512453b
Anna <annkrv80@yandex.ru>
2022-08-08 21:47:11 +0700

    поиск индекса заданного числа

---
+++ Task003/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1,44,3,4,55,64,55,8};
+
+int n=array.Length; //количество элементов в массиве
+int find =55;
+int index =0;
+
+while(index<n)
+{
+     if (array[index]==find)
+     {
+        Console.WriteLine(index);
+        break;// прерви после выполнения условия
+     }
+//index=index+1;
+index++;
+}
