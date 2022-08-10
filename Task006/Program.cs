b82d8b1905d426ab8474dfc0744d92d2b6b0a7f3
Anna <annkrv80@yandex.ru>
2022-08-10 23:08:53 +0700

    Вывод два числа,определить кратно ли второе число первому и если нет вывести остаток

---
+++ Task006/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int num1 = new Random().Next(10, 100);
+int num2 = new Random().Next(10, 100);
+int balance = num1 % num2;
+
+if (balance == 0)
+{
+    Console.WriteLine($"{num1} кратно {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} не кратно {num2} остаток {balance}");
+}
