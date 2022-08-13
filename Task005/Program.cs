78f05d54997c6c794897a56d87bf8a5bddb2d263
Anna <annkrv80@yandex.ru>
2022-08-13 19:54:16 +0700

    add task

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.Clear ();
- 
- int num = new Random().Next(100,1000);
- int a1=num/100;
- int a2=num%10;
+﻿Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a2 = num % 10;
 
 
 
