517b66b458d212a3ee755f8d5923228e24b8e666
Anna <annkrv80@yandex.ru>
2022-08-13 20:28:12 +0700

    Решение задачи

---
+++ Task018/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+int Q=0;
+try
+{
+   Console.Write("Введите номер четверти: ");
+   Q = int.Parse (Console.ReadLine()??"");
+}
+
+catch (Exception exc)
+{
+    Console.WriteLine($"Ошибка ввода данных {exc.Message}");
+}
+switch (Q)
+{
+    case 1:
+    Console.WriteLine ("X>0, Y>0");
+    break;
+    
+   case 2:
+    Console.WriteLine ("X<0, Y>0");
+    break;
+
+    case 3:
+    Console.WriteLine ("X>0, Y<0");
+    break;
+
+    case 4:
+    Console.WriteLine ("X<0, Y<0");
+    break;
+
+
+    default:
+    Console.WriteLine ("Неверное значение");
+    break;
+}
+
+
+
