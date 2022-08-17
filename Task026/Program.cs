e833a73f511dc76ee3cb16cf556c7dd517a3f479
Anna <annkrv80@yandex.ru>
2022-08-17 23:05:05 +0700

    Решение задачи

---
+++ Task026/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Console.Clear();
+ int num=GetNumberFromUser("Введите целое число А: ", "Ошибка ввода!");
+
+ int GetNumberFromUser(string message, string errorMessage)
+ {
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect=int.TryParse(Console.ReadLine(), out int userNumber);
+
+        if(isCorrect)
+        return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+ }
+
+Console.WriteLine("Количество цифр в числе");
+int cycleNumber=GetCycleNumber(num);
+Console.WriteLine($"\tВ числе {num} - {cycleNumber} цифр.");
+
+int GetCycleNumber(int number)
+{
+    int count=0;
+    
+   
+    while(number>0)
+    {
+        number/=10;
+        count++;
+    }
+    return count;
+}
