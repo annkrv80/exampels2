743dd8aaa4794290e1192c98415ad947e7c8dd3c
Anna <annkrv80@yandex.ru>
2022-08-26 19:39:26 +0700

    Вывод и заполнение двумерного массива

---
+++ Lektsia005/Program.cs
@@ -0,0 +1,31 @@
+﻿// Вывод и заполнение двумерного массива
+Console.Clear();
+
+int[,] matrix = new int[5,10];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+void PrintArray(int[,] matr)
+{ 
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+             matr[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
