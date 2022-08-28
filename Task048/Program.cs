0ed5275b25524563946f6b4c7487a04f4e299fd8
Anna <annkrv80@yandex.ru>
2022-08-28 22:15:34 +0700

    Двумерный массив

---
+++ Task048/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задайте двумерный массив размера m на n, 
+//каждый элемент в массиве находится по формуле: 
+//Aₘₙ = m+n. Выведите полученный массив на экран.
+
+Console.Clear();
+
+Console.Write("Введите число строк массива: ");
+int rows = int.Parse(Console.ReadLine()??"");
+Console.Write("Введите число столбцов в массиве: ");
+int colums=int.Parse(Console.ReadLine()?? "");
+int[,] array=GetArray(rows, colums);
+PrintArray(array);
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result= new int [m,n];
+
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j]=i+j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
