f1e771a79afc633e03ba0174208475d11fa796e3
Anna <annkrv80@yandex.ru>
2022-09-07 22:26:26 +0700

    Считает сумму цифр в числе

---
+++ Task067/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int num = GetNumberFromUser("Введите целое число:", "Ошибка вводаI");
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+   }
+
+}
+FindDigits(num);
+
+int FindDigits(int number)
+{
+    if(number/ 10 ==0) return number%10;
+    
+    else return FindDigits(number % 10)+ FindDigits(number/10);
+}
+
+Console.WriteLine($"сумма цифр число {num} равна {FindDigits(num)}");
