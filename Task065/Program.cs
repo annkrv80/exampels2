0d532ec3ba52813f8d8a51d2860582e60be3172e
Anna <annkrv80@yandex.ru>
2022-09-07 21:48:58 +0700

    Вывод чисел от А до К рекурсией

---
+++ Task065/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите целое число n1: ");
+int n1 = int.Parse(Console.ReadLine()??"");
+Console.WriteLine("Введите целое число n2: ");
+int n2 = int.Parse(Console.ReadLine()??"");
+
+PrintNumbers(n1,n2);
+
+
+void PrintNumbers(int start, int end)
+{
+
+    if (start == end +1) return;
+    if (start == end)
+    {
+        Console.Write(start + ". ");
+    }
+    else
+    {
+        Console.Write(start + ", ");
+    }
+    PrintNumbers(start + 1,  end);
+
+}
+
